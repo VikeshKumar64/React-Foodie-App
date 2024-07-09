a99b0ef7ebe45bd59c1fd62acdd132bac18d3e42
VikeshKumar64 <vikeshkumaroriginal@gmail.com>
2024-07-09 14:43:11 +0530

    Changes in Header as per React Routerv6 and above 09-07-2024

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -15,7 +15,7 @@ const Body = () =>{
     const ResturantCardOpen = isOpenLabel(RestaurantCard);
 
     const fetchData = async () => {
-        const data = await fetch(`${process.env.REACT_APP_API_PROXY_URL}https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
+        const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,45 @@
 import { useSelector } from "react-redux";
 import { C_LOGO } from "../utils/constants";
 import { Link, NavLink } from "react-router-dom";
- 
-const Header = () =>{
-    const cartItems = useSelector((store) =>store.cart.items);
+
+const Header = () => {
+    const cartItems = useSelector((store) => store.cart.items);
     console.log(cartItems);
 
     const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
-
-    return(
+    return (
         <div className="header">
             <div className="logo-container">
-                    <img className="logo" src={C_LOGO}/>
-            </div>  
+                <img className="logo" src={C_LOGO} />
+            </div>
 
-                <div className="nav-items">
-                    <ul>
+            <div className="nav-items">
+                <ul>
                     <li>
-                        <NavLink exact to="/" className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>Home</NavLink>
+                        <NavLink to="/" end className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>
+                            Home
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/about" className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>About us</NavLink>
+                        <NavLink to="/about" end className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>
+                            About us
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact" className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>Contact us</NavLink>
+                        <NavLink to="/contact" end className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>
+                            Contact us
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/cart"className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>
+                        <NavLink to="/cart" end className={({ isActive }) => (isActive ? "activ-link" : "linkss")}>
                             Cart({totalItems} Items)
                         </NavLink>
                     </li>
                 </ul>
-                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
